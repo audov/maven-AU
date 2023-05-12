2e636cb5f95ce63911c230871f07308e3a18f0a5
Neto test <audov@mail.ru>
2023-05-12 13:19:52 +0300

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import java.security.KeyStore;
+
+public class Main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+        long amount = 5000;
+        boolean registered = true;
+        long bonus = service.calculate(amount, registered);
+        System.out.println(bonus);
+    }
+}
