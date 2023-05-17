7bd88f54b8db98117264739bb62fe664ea750265
Neto test <audov@mail.ru>
2023-05-17 09:59:48 +0300

    code reformatted

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -52,20 +52,20 @@ public class BonusServiceTest {
     }
 
     @Test
-        void shouldCalculateForUnRegisteredAndOverLimit() {
-            BonusService service = new BonusService();
+    void shouldCalculateForUnRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
 
-            // подготавливаем данные:
-            long amount = 1_000_000;
-            boolean registered = false;
-            long expected = 500;
+        // подготавливаем данные:
+        long amount = 1_000_000;
+        boolean registered = false;
+        long expected = 500;
 
-            // вызываем целевой метод:
-            long actual = service.calculate(amount, registered);
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
 
-            // производим проверку (сравниваем ожидаемый и фактический):
-            Assertions.assertEquals(expected, actual);
-        }
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
 
     @Test
     void shouldCalculateForRegisteredAndEqualLimit() {
